f3a763f138dda9c035e254321b1215dac6f72747
Andrew Kim <sheehyun@outlook.com>
2025-12-04 14:28:27 -0500

    if top != -1 constructed

--- src/frontend_analysis/IR_analysis/RecursionRemoval.cpp
+++ src/frontend_analysis/IR_analysis/RecursionRemoval.cpp
@@ -222,10 +222,11 @@ DesignFlowStep_Status RecursionRemoval::InternalExec()
       first_block->PushBack(assignNeg1, AppM);
 
       // Build for loop to simulate recursion
-      //TODO This Fails
+      //TODO 
       const auto boolean_type = tree_man->GetBooleanType();
-      const tree_nodeRef cond = tree_man->create_binary_operation(boolean_type, top_var_identifier, neg1Cst, BUILTIN_SRCP, nop_expr_K);
+      const tree_nodeRef cond = tree_man->create_binary_operation(boolean_type, top_var_identifier, neg1Cst, BUILTIN_SRCP, ne_expr_K);
       const auto whileCond = tree_man->create_gimple_cond(cond, function_id, BUILTIN_SRCP);
+      first_block->PushFront(whileCond, AppM);
 
       // Rewrite operations around recursive call
       // TODO
