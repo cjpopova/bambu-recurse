126f2e62504a6b4f3d870b3436933e94e682f23f
cjpopova <casparpopova2023@u.northwestern.edu>
2025-12-18 13:02:45 -0500

    man fib annotations

--- examples/recurse/MLIR-benchmark/fib/man_fib.cpp
+++ examples/recurse/MLIR-benchmark/fib/man_fib.cpp
@@ -4,6 +4,7 @@ uint32_t man_fib(uint32_t n)
 {
     const unsigned DEPTH=512;
 
+    // BB2
     unsigned sp=0;
     uint32_t stack_n[DEPTH];
     uint32_t stack_r1[DEPTH];
@@ -14,19 +15,23 @@ uint32_t man_fib(uint32_t n)
     stack_state[sp]=0;
 
     while(1){
+        // BB4
         int state=stack_state[sp];
         uint32_t n=stack_n[sp];
 
         if(state==0){
-            if(n<=2){
+            if(n<=2){ // BB5
+                // BB6
                 //  return 1;
                 retval=1;
                 if(sp==0){
-                    break;
+                    break; // jump to BB15
                 }else{
+                    // BB 6B
                     sp--;
                 }
             }else{
+                // BB7
                 // call fib(n-1);
                 stack_state[sp]=1;
                 sp++;
@@ -34,6 +39,7 @@ uint32_t man_fib(uint32_t n)
                 stack_n[sp]=n-1;
             }
         }else if(state==1){
+            // BB8ish
             // completed r_fib(n-1)
             stack_r1[sp]=retval;
             // call fib(n-2);
@@ -43,15 +49,19 @@ uint32_t man_fib(uint32_t n)
             stack_n[sp]=n-2;
 
         }else if(state==2){
+            // BB8ish
             // completed r_fib(n-2)
             retval=stack_r1[sp]+retval;
             // return
             if(sp==0){
                 break;
             }else{
+                // BB8b
                 sp--;
             }
         }
+        // BB3: phis
     }
+    // BB15
     return retval;
 }
